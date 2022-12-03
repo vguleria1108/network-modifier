983d1e626d7ddabeeb20e1021d219b2213b28903
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2022-12-03 20:49:37 +0530

    UI added for adding target domain and query params

--- background.js
+++ background.js
@@ -1,23 +1,60 @@
-chrome.declarativeNetRequest.updateDynamicRules({
-  removeRuleIds: [1],
-  addRules: [
-    {
-      id: 1,
-      priority: 1,
-      action: {
-        type: "redirect",
-        redirect: {
-          transform: {
-            queryTransform: {
-              addOrReplaceParams: [{ key: "test", value: "123" }],
+(async function () {
+  let urlFilterObj = await chrome.storage.local.get("urlFilter");
+  let keyObj = await chrome.storage.local.get("key");
+  let valueObj = await chrome.storage.local.get("value");
+
+  let urlFilter = urlFilterObj.urlFilter ?? "";
+  let key = keyObj.key ?? "";
+  let value = valueObj.value ?? "";
+  chrome.storage.onChanged.addListener((changes, areaName) => {
+    if (areaName === "local") {
+      if (changes.urlFilter) {
+        urlFilter = changes.urlFilter.newValue ?? urlFilter;
+      }
+      if (changes.key) {
+        key = changes.key.newValue ?? key;
+      }
+      if (changes.value) {
+        value = changes.value.newValue ?? value;
+      }
+    }
+    updateDynamicRules({
+      urlFilter: urlFilter ?? "",
+      key: key ?? "",
+      value: value ?? "",
+    });
+  });
+  updateDynamicRules({
+    urlFilter: urlFilter ?? "",
+    key: key ?? "",
+    value: value ?? "",
+  });
+})();
+
+const updateDynamicRules = ({ urlFilter, key, value }) => {
+  console.log("updateDynamicRules" + urlFilter + "key=" + key + "val=" + value);
+  if (urlFilter && key && value)
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [1],
+      addRules: [
+        {
+          id: 1,
+          priority: 1,
+          action: {
+            type: "redirect",
+            redirect: {
+              transform: {
+                queryTransform: {
+                  addOrReplaceParams: [{ key: key, value: value }],
+                },
+              },
             },
           },
+          condition: {
+            urlFilter: urlFilter,
+            resourceTypes: ["xmlhttprequest"],
+          },
         },
-      },
-      condition: {
-        urlFilter: "https://app.c.nimblebox.ai/",
-        resourceTypes: ["xmlhttprequest"],
-      },
-    },
-  ],
-});
+      ],
+    });
+};
--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// Save the settings when the Save button is clicked
-document.getElementById("save-button").addEventListener("click", () => {
-  // Get the settings from the form fields
-  let queryParam = document.getElementById("query-param-field").value;
-  let queryValue = document.getElementById("query-value-field").value;
+(async function () {
+  let urlFilter = await chrome.storage.local.get("urlFilter");
+  let key = await chrome.storage.local.get("key");
+  let value = await chrome.storage.local.get("value");
 
-  // Save the settings to Chrome's local storage
-  chrome.storage.local.set({ queryParam, queryValue }, () => {
-    // Show a message to indicate that the settings were saved
-    document.getElementById("status").innerHTML = "Settings saved!";
+  document.getElementById("urlFilter").value = urlFilter?.urlFilter ?? "";
+  document.getElementById("key").value = key.key ?? "";
+  document.getElementById("value").value = value.value ?? "";
+  // Save the settings when the Save button is clicked
+  document.getElementById("save-button").addEventListener("click", () => {
+    // Get the settings from the form fields
+    let key = document.getElementById("key").value;
+    let value = document.getElementById("value").value;
+    let urlFilter = document.getElementById("urlFilter").value;
+
+    // Save the settings to Chrome's local storage
+    chrome.storage.local.set({ value, key, urlFilter }, () => {
+      // Show a message to indicate that the settings were saved
+      document.getElementById("status").innerHTML = "Settings saved!";
+    });
   });
-});
+})();
